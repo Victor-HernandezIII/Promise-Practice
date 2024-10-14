c3c6c809852b2e741729b5a6780ad87f5b8ec6d2
Victor Hernandez <garyoakiii@yahoo.com>
2024-10-14 07:38:00 -0500

    e1 Done

--- exercises/e6.js
+++ exercises/e6.js
@@ -16,6 +16,9 @@
 
 export const handlePromise = () => {
   // Your code goes here...
+  return Promise.reject("REJECTED!")
+    .catch(console.log)
+    .finally(() => console.log("This promise is finished!"));
 };
 
 // === TEST YOURSELF ===
